3f47f38ea7515c15c23278f4e18f3f07fc962bc1
Bhaweshgoyal <105268824+Bhaweshgoyal@users.noreply.github.com>
2023-02-25 17:58:16 +0530

    Delete Calender.react directory

--- Calender.react/store/store.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from "redux";
-import { reducers } from "../reducers/reducer";
-const store = createStore(reducers, window.__REDUX_DEVTOOL_EXTENSION__ && window.__REDUX_DEVTOOL_EXTENSION__())
-export default  store
